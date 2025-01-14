dd0d86eb0fa8374950169c6df0c4dc4cb94a4dd3
Bryant Biggs <bryantbiggs@gmail.com>
2025-01-14 16:12:17 -0600

    chore: Add initial configs from `cookiecluster`

---
+++ variables.tf
@@ -0,0 +1,9 @@
+
+################################################################################
+# Variables - Required input
+################################################################################
+
+variable "capacity_reservation_id" {
+  description = "The ID of the ML capacity block reservation in which to run the instance(s)"
+  type        = string
+}
